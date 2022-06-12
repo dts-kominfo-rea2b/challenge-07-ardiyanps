18aaeeb79fcd4aa002266d823ea27b31b89a5640
Ardiyan puji santoso <40789455+ardiyanps@users.noreply.github.com>
2022-06-12 17:04:57 +0700

    Bismillah merchandise

--- promise.js
+++ promise.js
@@ -1,7 +1,18 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    const dataIXX = await promiseTheaterIXX();
+    const dataVGC = await promiseTheaterVGC();
+
+    hasil = dataIXX.concat(dataVGC);
+
+    return hasil.filter((x) => x.hasil == emosi).length;
+  } catch (error) {
+    return console.log(error);
+  }
+};
 
 module.exports = {
   promiseOutput,
